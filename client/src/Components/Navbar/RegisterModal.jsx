8c00779b97fbc6a8eef1f7f77c0bae2a1e53ef69
Sukma <aspriliyawan321@gmail.com>
2021-08-05 11:39:55 +0700

    add-first

---
+++ client/src/Components/Navbar/RegisterModal.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { registerService } from "../../services/httpServices";
+import Modal from "../ModalAndPopup/Modal";
+import Popup from "../ModalAndPopup/Popup";
+
+function RegisterModal({ show, setShow, switchModal, dispatch }) {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    fullName: "",
+  });
+  const [showPop, setShowPop] = useState(false);
+  const onSwitch = () => {
+    setShow(false);
+    switchModal(true);
+  };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const onRegister = (e) => {
+    e.preventDefault();
+    registerService(dispatch, formData, setShowPop);
+  };
+  return (
+    <Modal show={show} setShow={setShow}>
+      <div className="p-modal">
+        <div className="w-modal">
+          <h2 className="text-modal-title">Register</h2>
+          <form className="">
+            <input
+              name="email"
+              placeholder="Email"
+              className="input w-100"
+              onChange={(e) => handleChange(e)}
+              value={formData.email}
+            />
+            <input
+              name="password"
+              placeholder="Password"
+              type="password"
+              className="input w-100"
+              onChange={(e) => handleChange(e)}
+              value={formData.password}
+            />
+            <input
+              name="fullName"
+              placeholder="Full Name"
+              className="input w-100"
+              onChange={(e) => handleChange(e)}
+              value={formData.fullName}
+            />
+            <button
+              className="btn btn-primary w-100 btn-modal mt-28"
+              onClick={onRegister}
+            >
+              Register
+            </button>
+            <p className="text-center text-modal mt-21">
+              Already have an account ?{" "}
+              <span className="half-bold cursor" onClick={onSwitch}>
+                Klik Here
+              </span>
+            </p>
+          </form>
+        </div>
+      </div>
+      <Popup
+        text="Please Input Detail"
+        type="danger"
+        show={showPop}
+        setShow={setShowPop}
+      />
+    </Modal>
+  );
+}
+
+export default RegisterModal;
---
+++ client/src/Components/Utils/ProductBox.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import format from "./formatingCurency";
+import logo from "../../Images/Icon.png";
+import { getProductByIdService, baseURL } from "../../services/httpServices";
+
+function ProductBox({ children, dataProduct, ready = false }) {
+  const [product, setProducts] = useState(null);
+  const qty = ready ? dataProduct.orderQuantity.value : dataProduct.qty;
+  useEffect(() => {
+    if (ready) {
+      setProducts(dataProduct);
+    } else if (dataProduct) {
+      (async () => {
+        const data = await getProductByIdService(dataProduct.id);
+        setProducts(data.data.data.product);
+      })();
+    }
+  }, [dataProduct, ready]);
+
+  return product ? (
+    <div className="space-between bg-back shipping-card-con">
+      <div className="row">
+        <div className="align-center">
+          <img
+            src={`${baseURL}${product.photo}`}
+            className="shipping-card-img"
+            alt={product.name}
+          />
+        </div>
+        <div className="ml-13">
+          <h5 className="shipping-card-name">{product.name}</h5>
+          <p className="shipping-card-desc">
+            <span className="half-bold">Thursday</span>, 5 August 2021
+          </p>
+          <p className="shipping-card-desc mt-21">
+            Price : {format(+product.price)}
+          </p>
+          <p className="shipping-card-desc">Qty : {qty}</p>
+          <p className="shipping-card-desc">
+            <span className="half-bold">
+              Sub Total : {format(+product.price * +qty)}
+            </span>
+          </p>
+        </div>
+      </div>
+      <div>
+        <div className="item-center">
+          <img src={logo} className="shipping-card-logo" alt={"logo"} />
+        </div>
+        <div>{children}</div>
+      </div>
+    </div>
+  ) : null;
+}
+
+export default ProductBox;
